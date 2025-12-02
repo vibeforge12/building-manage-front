ec02dc5d08251919f6ee71f29ccfe76f22d4c81c
sw <sunwoo05091@naver.com>
2025-12-02 16:47:27 +0900

    google plist 교체

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,10 +10,11 @@ plugins {
 
 android {
     namespace = "com.example.building_manage_front"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    compileSdk = 35
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
@@ -27,10 +28,11 @@ android {
         applicationId = "com.example.building_manage_front"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        multiDexEnabled = true
     }
 
     buildTypes {
@@ -44,4 +46,8 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
